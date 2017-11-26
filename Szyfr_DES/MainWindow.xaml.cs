bffb2c7b060c62130289b4b8701e76e29836aff6
Madnarth <corwyn.blade@gmail.com>
2017-11-26 15:18:26 +0100

    Pierwszy commit

---
+++ Szyfr_DES/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Szyfr_DES
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        char[] bytesArr;
+        static byte[] bytes;
+        #region Metoda szyfrowania
+        public static string Encrypt(string originalString)
+        {
+            if (String.IsNullOrEmpty(originalString))
+            {
+                throw new ArgumentNullException
+                       ("Tekst do zaszyfrowania nie może być null.");
+            }
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                cryptoProvider.CreateEncryptor(bytes, bytes), CryptoStreamMode.Write);
+            StreamWriter writer = new StreamWriter(cryptoStream);
+            writer.Write(originalString);
+            writer.Flush();
+            cryptoStream.FlushFinalBlock();
+            writer.Flush();
+            return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+        }
+        #endregion
+        #region Metoda deszyfrowania
+        public static string Decrypt(string cryptedString)
+        {
+            if (String.IsNullOrEmpty(cryptedString))
+            {
+                throw new ArgumentNullException
+                   ("Tekst do rozszyfrowania nie może być null.");
+            }
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            MemoryStream memoryStream = new MemoryStream
+                    (Convert.FromBase64String(cryptedString));
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                cryptoProvider.CreateDecryptor(bytes, bytes), CryptoStreamMode.Read);
+            StreamReader reader = new StreamReader(cryptoStream);
+            return reader.ReadToEnd();
+        } 
+        #endregion
+        private void szyfruj(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bytesArr = TxtBoxKlucz.Text.ToCharArray();
+                ASCIIEncoding.ASCII.GetBytes(bytesArr);
+                bytes = ASCIIEncoding.ASCII.GetBytes(bytesArr);
+                TxtBoxSzyf.Text = Encrypt(TxtBoxNieszyf.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void deszyfruj(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                TxtBoxRoszyf.Text = Decrypt(TxtBoxZaszyf.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ButtKopiuj_Click(object sender, RoutedEventArgs e)
+        {
+            TxtBoxZaszyf.Text = TxtBoxSzyf.Text;
+        }
+    }
+}
